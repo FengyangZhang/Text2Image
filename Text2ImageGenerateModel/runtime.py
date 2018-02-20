2c4e75626e2f70b7b3e52a830658013583b1ffc6
FengyangZhang <zhfy888@163.com>
2018-02-19 22:57:34 -0500

    add validation code

--- Text2ImageGenerateModel/runtime.py
+++ Text2ImageGenerateModel/runtime.py
@@ -1,4 +1,5 @@
 from trainer import Trainer
+from validate import Validation
 import argparse
 from PIL import Image
 import os
@@ -20,26 +21,39 @@ parser.add_argument('--split', default=0, type=int)
 parser.add_argument('--batch_size', default=64, type=int)
 parser.add_argument('--num_workers', default=8, type=int)
 parser.add_argument('--epochs', default=200, type=int)
+# if specified, run in validation mode
+parser.add_argument('--val', default=False, action='store_true')
 args = parser.parse_args()
 
-trainer = Trainer(type=args.type,
-                  dataset=args.dataset,
-                  split=args.split,
-                  lr=args.lr,
-                  diter=args.diter,
-                  vis_screen=args.vis_screen,
-                  save_path=args.save_path,
-                  l1_coef=args.l1_coef,
-                  l2_coef=args.l2_coef,
-                  pre_trained_disc=args.pre_trained_disc,
-                  pre_trained_gen=args.pre_trained_gen,
-                  batch_size=args.batch_size,
-                  num_workers=args.num_workers,
-                  epochs=args.epochs
-                  )
+if not args.val:
+  trainer = Trainer(type=args.type,
+                    dataset=args.dataset,
+                    split=args.split,
+                    lr=args.lr,
+                    diter=args.diter,
+                    vis_screen=args.vis_screen,
+                    save_path=args.save_path,
+                    l1_coef=args.l1_coef,
+                    l2_coef=args.l2_coef,
+                    pre_trained_disc=args.pre_trained_disc,
+                    pre_trained_gen=args.pre_trained_gen,
+                    batch_size=args.batch_size,
+                    num_workers=args.num_workers,
+                    epochs=args.epochs
+                    )
 
-if not args.inference:
-    trainer.train(args.cls)
+  if not args.inference:
+      trainer.train(args.cls)
+  else:
+      trainer.predict()
+
 else:
-    trainer.predict()
-
+  validation = Validation(type=args.type,
+                    dataset=args.dataset,
+                    vis_screen=args.vis_screen,
+                    pre_trained_gen=args.pre_trained_gen,
+                    batch_size=args.batch_size,
+                    num_workers=args.num_workers,
+                    epochs=args.epochs
+                    )
+  validation.validate()
